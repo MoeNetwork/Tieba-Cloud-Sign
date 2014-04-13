56dcf8ca73911abcb23323215147aba1382342b9
无名智者 <kenvix@vip.qq.com>
2014-04-13 13:33:31 +0800

    增加强制MySQL方式连接到数据库功能

--- init.php
+++ init.php
@@ -1,6 +1,7 @@
 <?php
 define('SYSTEM_FN','百度贴吧云签到');
 define('SYSTEM_VER','1.0');
+define('SQLMODE', 'mysqli');
 define('SYSTEM_ROOT',dirname(__FILE__));
 define('SYSTEM_PAGE',isset($_REQUEST['mod']) ? strip_tags($_REQUEST['mod']) : 'default');
 $PluginHooks = array();
@@ -8,7 +9,7 @@ $today       = date("Y-m-d");
 header("content-type:text/html; charset=utf-8");
 require SYSTEM_ROOT.'/setup/msg.php';
 require SYSTEM_ROOT.'/config.php';
-if (class_exists("mysqli")) {
+if (class_exists("mysqli") && SQLMODE != 'mysql') {
 	require SYSTEM_ROOT.'/mysqli.php';
 } else {
 	require SYSTEM_ROOT.'/mysql.php';
