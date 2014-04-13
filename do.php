d62f40a5c7c3c5054229706fa7dbb84c3cca89e8
无名智者 <kenvix@vip.qq.com>
2014-04-13 10:31:56 +0800

    删除 [签到进程锁] 功能

--- do.php
+++ do.php
@@ -4,8 +4,6 @@ require 'init.php';
 require SYSTEM_ROOT.'/sign.functions.php';
 set_time_limit(0);
 global $m,$today;
-if (option::get('cron_isdoing') == 0) {
-	option::set('cron_isdoing',1);
 	$return = '';
 	doAction('cron_1');
 
@@ -28,9 +26,5 @@ if (option::get('cron_isdoing') == 0) {
 
 	$count = $m->fetch_row($m->query("SELECT COUNT(*) FROM `".DB_NAME."`.`".DB_PREFIX."tieba` WHERE `lastdo` != '".$today."'"));
 	doAction('cron_2');
-	option::set('cron_isdoing',0);
 	msg('本次计划任务完毕',false,false);
-} else {
-	msg('已经有一个计划任务正在运行中，本次运行已被系统阻止',false,false);
-}
 ?>
