18ec402b343cefc31e251be58ba292a80546bb80
无名智者 <kenvix@vip.qq.com>
2014-04-18 19:23:55 +0800

    内置功能增强，V2就快到了

--- class.php
+++ class.php
@@ -1,6 +1,5 @@
 <?php
 if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); } 
-
 /**
  * option 设置类
  */
@@ -72,7 +71,7 @@ class option {
 class cron Extends option {
 	/**
 	 * 获取计划任务名称
-	 * $name 计划任务名称
+	 * @param $name 计划任务名称
 	 * @return array
 	*/
 	public static function get($name) {
@@ -83,23 +82,80 @@ class cron Extends option {
 	/**
 	 * 改变或添加计划任务 (不存在时自动添加)
 	 * $name 计划任务名称
-	 * $file 计划任务文件，执行时以include方式执行
+	 * $file 计划任务文件，执行时以include方式执行function，function名称为cron_计划任务名称
 	 * $no 忽略任务
 	 * $status 计划任务状态，系统会写入
 	 * $freq 执行频率
+	 *       -1：一次性任务，执行完毕后系统会删除
+	 *       0 ：默认，当do.php被执行时，该任务始终被运行
+	 *       其他正整数：运行时间间隔，单位秒（$lastdo - $freq）
 	 * $lastdo 上次执行，系统会写入
 	 * $log 执行日志，系统会写入
 	*/
 	public static function set($name, $file = '', $no = 0, $status = 0, $freq = 0, $lastdo = '', $log = '') {
 		global $m;
-		$x = $m->once_fetch_array("SELECT COUNT(*) AS ffffff FROM `".DB_NAME."`.`".DB_PREFIX."options` WHERE `name` = '{$name}'");
+		$x = $m->once_fetch_array("SELECT COUNT(*) AS ffffff FROM `".DB_NAME."`.`".DB_PREFIX."cron` WHERE `name` = '{$name}'");
 		if ($x['ffffff'] <= 0) {
 			$m->query("INSERT INTO  `".DB_NAME."`.`".DB_PREFIX."cron` (`id`, `name`, `file`, `no`, `status`, `freq`, `lastdo`, `log`) VALUES (NULL, '{$name}', '{$file}', '{$no}', '{$status}', '{$freq}', '{$lastdo}', '{$log}');");	
 		} else {
 			$m->query("UPDATE  `".DB_NAME."`.`".DB_PREFIX."cron` SET  `name` =  '{$name}',`file` =  '{$file}',`no` =  '{$no}',`status` =  '{$status}',`freq` =  '{$freq}',`lastdo` =  '{$lastdo}',`log` =  '{$log}'  WHERE `name` = '{$name}'");
 		}
 	}
+
+	/**
+	 * 删除一个计划任务
+	 * @param $name 计划任务名称
+	*/
+	public static function del($name) {
+		global $m;
+		$m->query("DELETE FROM `".DB_NAME."`.`".DB_PREFIX."cron` WHERE `name` = '{$name}'");
+	}
 }
 
+/**
+ * 杂项功能类
+ */
 
+class misc {
+	/**
+	 * 快捷发送一封邮件
+	 * @param $to 收件人
+	 * @param $sub 邮件主题
+	 * @param $msg 邮件内容
+	 * @param $att 数组，附件的路径，可以多个附件，例如array('/plugins/wmzz_mailer/demo.jpg','/plugins/wmzz_mailer/f.jpg')
+	 * @return 成功:true 失败：错误消息
+	 */
+	public static function mail($to, $sub = '无主题', $msg = '无内容', $att = array()) {
+		$mail = new PHPMailer();
+		if (option::get('mail_mode') == 'SMTP') {
+			$mail->isSMTP();
+			$mail->Mailer = 'SMTP';
+			$mail->SMTPDebug = 0;
+			$mail->Debugoutput = 'html';
+			$mail->Host = option::get('mail_host');
+			$mail->Port = option::get('mail_port');
+			$mail->SMTPAuth = (boolean) option::get('mail_auth');
+			$mail->Username = option::get('mail_smtpname');
+			$mail->Password = option::get('mail_smtppw');
+		} else {
+			$mail->Mailer = 'MAIL';
+		}
+			$mail->CharSet = "UTF-8"; //核心代码，可以解决乱码问题
+			$mail->setFrom(option::get('mail_name'), option::get('mail_yourname'));
+			$mail->addReplyTo(option::get('mail_name'), option::get('mail_yourname'));
+			$mail->addAddress($to, $to);
+			$mail->Subject = $sub;
+			$mail->Body = $msg;
+			$mail->msgHTML = $msg;
+			$mail->AltBody = $msg;
+			foreach ($att as $value) {
+				$mail->addAttachment($value);
+			}
+		    if(!$mail->Send()) {
+		        return $mail->ErrorInfo;
+		    } else {
+		       	return true;
+		    }
+	}
+}
 ?>
--- init.php
+++ init.php
@@ -9,13 +9,14 @@ header("content-type:text/html; charset=utf-8");
 require SYSTEM_ROOT.'/setup/msg.php';
 require SYSTEM_ROOT.'/config.php';
 require SYSTEM_ROOT.'/mysql_autoload.php';
-require SYSTEM_ROOT.'/option.php';
+require SYSTEM_ROOT.'/lib/PHPMailerAutoload.php';
+require SYSTEM_ROOT.'/class.php';
 new option();
 define('SYSTEM_URL',option::get('system_url'));
 if (option::get('dev') != 1) { error_reporting(0); }
-require SYSTEM_ROOT.'/protector.php';
 require SYSTEM_ROOT.'/sfc.functions.php';
 require SYSTEM_ROOT.'/ui.php';
 require SYSTEM_ROOT.'/globals.php';
+require SYSTEM_ROOT.'/protector.php';
 require SYSTEM_ROOT.'/plugins.php';
 ?>
