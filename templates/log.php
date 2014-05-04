71b8a51751d58b7b2fb1e7b9f5911e895e565744
无名智者 <kenvix@vip.qq.com>
2014-05-04 13:59:35 +0800

    修复签到日志无法计算并显示分表内的贴吧的BUG

--- templates/log.php
+++ templates/log.php
@@ -3,11 +3,11 @@ global $m,$today;
 if (BDUSS == null) {
 	echo '<div class="alert alert-danger">无法列出签到日志，因为当前没有绑定百度账号</div>';	
 }
-	$count1 = $m->fetch_row($m->query("SELECT COUNT(*) FROM `".DB_NAME."`.`".DB_PREFIX."tieba` WHERE `lastdo` = '".$today."' AND `uid` = ".UID));
+	$count1 = $m->fetch_row($m->query("SELECT COUNT(*) FROM `".DB_NAME."`.`".DB_PREFIX.TABLE."` WHERE `lastdo` = '".$today."' AND `uid` = ".UID));
 	$count1 = $count1[0];
-	$count2 = $m->fetch_row($m->query("SELECT COUNT(*) FROM `".DB_NAME."`.`".DB_PREFIX."tieba` WHERE `lastdo` != '".$today."' AND `uid` = ".UID));
+	$count2 = $m->fetch_row($m->query("SELECT COUNT(*) FROM `".DB_NAME."`.`".DB_PREFIX.TABLE."` WHERE `lastdo` != '".$today."' AND `uid` = ".UID));
 	$count2 = $count2[0];
-	$q=$m->query("SELECT * FROM  `".DB_NAME."`.`".DB_PREFIX."tieba` WHERE `uid` = ".UID);
+	$q=$m->query("SELECT * FROM  `".DB_NAME."`.`".DB_PREFIX.TABLE."` WHERE `uid` = ".UID);
 	echo "<div class=\"alert alert-info\">已签到 {$count1} 个贴吧，还有 {$count2} 个贴吧等待签到。<a href=\"index.php?mod=showtb\">设置云签到</a><br/>如果某个贴吧签到状态为异常，可点击该链接查看详情</div>";
 	echo '<table class="table"><thead><tr><th style="width:8%">ID</th><th style="width:52%">贴吧名称</th><th style="width:16%">状态</th><th style="width:24%">上次签到</th></thead><tbody>';
 while ($x=$m->fetch_array($q)) {
