4b74e8546ea0b0d017e48f597e995d49692ee1f8
无名智者 <kenvix@vip.qq.com>
2014-05-22 12:37:51 +0800

    增加用户自定义CSS/JS功能

--- ui.php
+++ ui.php
@@ -5,12 +5,14 @@ function loadhead() {
 	echo '<!DOCTYPE html><html><head>';
 	echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-	echo '<title>'.SYSTEM_FN.'</title><meta name="generator" content="God.Kenvix\'s Blog (http://zhizhe8.net) and StusGame GROUP (http://www.stus8.com)" /></head><body>';
+	echo '<title>'.SYSTEM_NAME.'</title><meta name="generator" content="God.Kenvix\'s Blog (http://zhizhe8.net) and StusGame GROUP (http://www.stus8.com)" /></head><body>';
 	echo '<script src="'.SYSTEM_URL.'js/jquery.min.js"></script>';
 	echo '<link rel="stylesheet" href="'.SYSTEM_URL.'css/bootstrap.min.css">';
 	echo '<script src="'.SYSTEM_URL.'js/bootstrap.min.js"></script>';
 	echo '<style type="text/css">body { font-family:"微软雅黑","Microsoft YaHei";background: #eee; }</style>';
 	echo '<script type="text/javascript" src="'.SYSTEM_URL.'js/js.js"></script>';
+	echo '<link rel="stylesheet" href="'.SYSTEM_URL.'css/my.css">';
+	echo '<script type="text/javascript" src="'.SYSTEM_URL.'js/my.js"></script>';
 	doAction('header');
 	if (option::get('trigger') == 1) {
 		echo "<script>$.ajax({ async:true, url: '".SYSTEM_URL."do.php', type: 'GET', data : {},dataType: 'HTML'});</script>";
