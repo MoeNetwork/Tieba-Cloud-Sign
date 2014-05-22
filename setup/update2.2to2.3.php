7d0dbac2888a21aa9fe7f44e97f0d1fb4a51a791
无名智者 <kenvix@vip.qq.com>
2014-05-22 14:33:02 +0800

    V2.9:2.x时代的最后一个版本，此版本升级需要运行setup/update2.2to2.3.php

--- setup/update2.2to2.3.php
+++ setup/update2.2to2.3.php
@@ -2,11 +2,11 @@
 require '../init.php';
 global $m;
 if (ROLE == 'admin') {
-	$m->query("INSERT INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'system_name', '贴吧云签到');");
-	$m->query("INSERT INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'cron_sign_again', '');");
-	$m->query("INSERT INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'cron_order', '1');");
-	$m->query("INSERT INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'sign_mode', 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"2\";}');");
-	$m->query("ALTER TABLE `".DB_PREFIX."cron` ADD  `orde` INT( 10 ) NOT NULL DEFAULT  '0' AFTER  `name` ;");
+	$m->query("UPDATE `".DB_PREFIX."options` SET `value` = '贴吧云签到' WHERE `".DB_PREFIX."options`.`name` = 'system_name';");
+	$m->query("REPLACE INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'cron_sign_again', '');");
+	$m->query("REPLACE INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'cron_order', '1');");
+	$m->query("REPLACE INTO `".DB_PREFIX."options` (`id`, `name`, `value`) VALUES (NULL, 'sign_mode', 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"2\";}');");
+	$m->query("ALTER TABLE `".DB_PREFIX."cron` ADD  `orde` INT( 10 ) NOT NULL DEFAULT  '0' AFTER  `name` ;",true);
 	unlink(__FILE__);
 	msg('您的云签到已成功升级到 V2.3 版本，请立即删除此文件，谢谢');
 } else {
