1f9ec55beffd0dc0ded72540ab8ad853bf388deb
无名智者 <kenvix@vip.qq.com>
2014-06-10 14:10:59 +0800

    【紧急补丁】修复所有模式无法签到的问题，升级后务必运行setup/update2.9to2.91.php升级补丁

--- setup/update2.9to2.91.php
+++ setup/update2.9to2.91.php
@@ -8,7 +8,7 @@ if (ROLE == 'admin') {
 	//捕获自增ID作为PID，更新贴吧数据表
 	$m->query("UPDATE `".DB_PREFIX."tieba` SET  `lastdo` =  '2010-10-10' WHERE `".DB_PREFIX."tieba`.`lastdo` != '2010-10-10';");
 	foreach ($fbs as $value) {
-		$m->query("UPDATE `".DB_PREFIX."tieba` SET  `lastdo` =  '2010-10-10' WHERE `".DB_PREFIX.$value."`.`lastdo` != '2010-10-10';");
+		$m->query("UPDATE `".DB_PREFIX.$value."` SET  `lastdo` =  '2010-10-10' WHERE `".DB_PREFIX.$value."`.`lastdo` != '2010-10-10';");
 	}
 	unlink(__FILE__);
 	msg('您的云签到已成功升级到 V2.91 版本，请立即删除此文件，谢谢');
