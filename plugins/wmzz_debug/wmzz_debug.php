f5e6f219ec318246d0b78640e9f41a97c78f4b4d
无名智者 <kenvix@vip.qq.com>
2014-09-10 22:20:42 +0800

    [debug插件]保留PHP运行耗时的小数点后6位

--- plugins/wmzz_debug/wmzz_debug.php
+++ plugins/wmzz_debug/wmzz_debug.php
@@ -25,7 +25,7 @@ addAction('index_3','wmzz_debug_phpinfo');
 
 function wmzz_debug_system2() {
 	global $m;
-	echo '<br/>调试信息：执行 MySQL 查询 '. $m->queryCount . ' 次，PHP 运行耗时 '. (microtime() - $GLOBALS['wmzz_debug_time']) . ' 秒';
+	echo '<br/>调试信息：执行 MySQL 查询 '. $m->queryCount . ' 次，PHP 运行耗时 '. round(microtime() - $GLOBALS['wmzz_debug_time'],6) . ' 秒';
 }
 
 addAction('footer','wmzz_debug_system2');
