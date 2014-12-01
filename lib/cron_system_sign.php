0596d82f2ad1d5c3bb921a8f7028fd458c128de8
无名智者 <kenvix@vip.qq.com>
2014-12-01 23:27:44 +0800

    修复无法重复签到的bug

--- lib/cron_system_sign.php
+++ lib/cron_system_sign.php
@@ -9,7 +9,7 @@ if (!defined('SYSTEM_ROOT')) { die('Insufficient Permissions'); }
 function cron_system_sign() {
 	global $i;
 
-	$today = date('Y-m-d H:m:s');
+	$today = date('Y-m-d');
 
 	$sign_again = unserialize(option::get('cron_sign_again'));
 	if ($sign_again['lastdo'] != $today) {
