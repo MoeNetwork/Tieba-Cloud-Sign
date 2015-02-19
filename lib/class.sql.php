8d68dff3c992940e0c653c9f62b5e7c8d29ac08d
无名智者 <kenvix@vip.qq.com>
2015-02-19 16:12:33 +0800

    增加sql类

---
+++ lib/class.sql.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * 数据库类
+ * @copyright (c) 无名智者
+ */
+
+class sql extends wmysql {
+
+	/**
+	 * 构造函数
+	 */
+	public function __construct($host , $user , $pw , $name) {
+		parent::__construct($host , $user , $pw , $name);
+	}
+
+	/**
+	 * 添加列，如果存在但列类型不一样则为更改列类型，如果存在且列类型一样则忽略
+	 * @param string $table  表名，不需要带前缀
+	 * @param string $column 列名
+	 * @param string $type 列类型，如varchar(10)
+	 * @param string $other 其他信息，如NOT NULL
+	 * @return mysql_result|bool 忽略返回false
+	 */
+	public function addColumn($table , $column , $type , $other = '') {
+		$table = self::_prefix($table);
+		$d = $this->once_fetch_array("Describe `{$table}` `{$column}`");
+		if (empty($d['Field'])) {
+			return $this->query("ALTER TABLE `{$table}` ADD COLUMN `{$column}` {$type} {$other}");
+		} elseif ($d['Type'] != $type) {
+			return $this->query("ALTER TABLE `{$table}` MODIFY COLUMN `{$column}` {$type} {$other}");
+		} 
+		return false;
+	}
+
+	/**
+	 * 如果存在，则移除列
+	 * @param string $table  表名，不需要带前缀
+	 * @param string $column 列名
+	 * @return mysql_result|bool 忽略返回false
+	 */
+	public function delColumn($table , $column) {
+		$table = self::_prefix($table);
+		$d = $this->fetch_row($this->query("Describe `{$table}` `{$column}`"));
+		if (!empty($d[0])) {
+			return $this->query("ALTER TABLE `{$table}` DROP COLUMN `{$column}`");
+		}
+		return false;
+	}
+
+	/**
+	 * 添加索引，存在则忽略
+	 * @param string $table  表名，不需要带前缀
+	 * @param string $index  索引名
+	 * @param string $column 列名
+	 * @param int $type 索引类型，0为普通索引，1为FULLTEXT，2为UNIQUE
+	 * @param int $method 索引方法，0为普通B-TREE，1为HASH
+	 * @return mysql_result|bool 忽略返回false
+	 */
+	public function addIndex($table , $index , $column , $type = 0 , $method = 0) {
+		$table   = self::_prefix($table);
+		$typee   = self::getIndexType($type);
+		$methodd = self::getIndexMethod($method);
+		$d = $this->once_fetch_array("SHOW INDEX FROM `{$table}` WHERE `Key_name` = '{$index}'");
+		if (empty($d['Key_name'])) {
+			return $this->query("ALTER TABLE `{$table}` ADD {$typee} INDEX `{$index}` ({$column}) USING {$methodd}");
+		}
+		return false;
+	}
+
+	/**
+	 * 删除索引，不存在则忽略
+	 * @param string $table  表名，不需要带前缀
+	 * @param string $index  索引名
+	 * @return mysql_result|bool 忽略返回false
+	 */
+	public function delIndex($table , $index) {
+		$table = self::_prefix($table);
+		$d = $this->fetch_row($this->query("SHOW INDEX FROM `{$table}` WHERE `Key_name` = '{$index}'"));
+		if (!empty($d[0])) {
+			return $this->query("ALTER TABLE `{$table}` DROP INDEX `{$index}`");
+		}
+		return false;
+	}
+
+	/**
+	 * 获取索引类型
+	 * @param int $type 索引类型ID
+	 * @return  string
+	 */
+	public static function getIndexType($type) {
+		if ($type == 1) {
+			return 'FULLTEXT';
+		} elseif ($type == 2) {
+			return 'UNIQUE';
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * 获取索引方法
+	 * @param int $id 索引方法ID
+	 * @return  string
+	 */
+	public static function getIndexMethod($id) {
+		if ($type == 1) {
+			return 'HASH';
+		} else {
+			return 'BTREE';
+		}
+	}
+
+	/**
+	 * 给表名加前缀
+	 * @param string $table  表名，不需要带前缀
+	 * @return 有前缀的表名
+	 */
+	private function _prefix($table) {
+		return DB_PREFIX . $table;
+	}
+
+}
--- setup/install.template.sql
+++ setup/install.template.sql
@@ -1,14 +1,18 @@
+SET FOREIGN_KEY_CHECKS=0;
+
 DROP TABLE IF EXISTS `{VAR-PREFIX}baiduid`;
 CREATE TABLE `{VAR-PREFIX}baiduid` (
-`id` int(30) NOT NULL COMMENT 'pid',
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `uid` int(30) NOT NULL,
   `bduss` text,
-  `name` varchar(40) DEFAULT NULL
+  `name` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `{VAR-PREFIX}cron`;
 CREATE TABLE `{VAR-PREFIX}cron` (
-`id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `orde` int(10) NOT NULL DEFAULT '0',
   `file` varchar(1000) DEFAULT NULL,
@@ -16,27 +20,84 @@ CREATE TABLE `{VAR-PREFIX}cron` (
   `desc` text,
   `freq` int(10) NOT NULL DEFAULT '0',
   `lastdo` varchar(100) DEFAULT NULL,
-  `log` text
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  `log` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+INSERT INTO `{VAR-PREFIX}cron` VALUES ('1', 'system_sign', '0', 'lib/cron_system_sign.php', '0', '每天对所有贴吧进行签到\r\n忽略或卸载此任务会导致停止签到', '0', '0', '');
+INSERT INTO `{VAR-PREFIX}cron` VALUES ('2', 'system_sign_retry', '1', 'lib/cron_system_sign_retry.php', '0', '对所有签到失败的贴吧进行复签\r\n忽略或卸载此任务会导致停止复签', '0', '0', '');
 
 DROP TABLE IF EXISTS `{VAR-PREFIX}options`;
 CREATE TABLE `{VAR-PREFIX}options` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `value` text
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  `value` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+
+INSERT INTO `{VAR-PREFIX}options` VALUES ('1', 'system_url', '{VAR-SYSTEM-URL}');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('2', 'system_name', '贴吧云签到');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('3', 'protector', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('4', 'cron_limit', '15');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('5', 'tb_max', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('6', 'sign_mode', 'a:2:{i:0;s:1:\"1\";i:1;s:1:\"3\";}');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('7', 'footer', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('8', 'enable_reg', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('9', 'protect_reg', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('10', 'yr_reg', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('11', 'icp', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('12', 'trigger', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('13', 'mail_mode', 'MAIL');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('14', 'mail_name', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('15', 'mail_yourname', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('16', 'mail_host', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('17', 'mail_port', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('18', 'mail_auth', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('19', 'mail_smtpname', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('20', 'mail_smtppw', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('21', 'fb', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('22', 'cloud', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('23', 'enable_addtieba', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('24', 'pwdmode', 'md5(md5(md5($pwd)))');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('25', 'retry_max', '10');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('26', 'cron_order', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('27', 'fb_tables', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('28', 'dev', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('29', 'cron_last_do_time', '2015-02-13');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('30', 'cron_last_do', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('31', 'cron_isdoing', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('32', 'cron_pw', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('33', 'sign_sleep', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('34', 'cktime', '999999');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('35', 'bduss_num', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('36', 'sign_multith', '1');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('37', 'sign_asyn', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('38', 'cron_asyn', '');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('39', 'cron_sign_again', 'a:2:{s:3:\"num\";i:0;s:6:\"lastdo\";s:10:\"2015-02-13\";}');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('40', 'sign_hour', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('41', 'mail_ssl', '0');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('42', 'freetable', 'tieba');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('43', 'core_version', '3.9');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('44', 'isapp', '{VAR-ISAPP}');
+INSERT INTO `{VAR-PREFIX}options` VALUES ('45', 'baidu_name', '1');
 
 DROP TABLE IF EXISTS `{VAR-PREFIX}plugins`;
 CREATE TABLE `{VAR-PREFIX}plugins` (
-`id` int(10) NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '0',
-  `options` text
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  `options` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `{VAR-PREFIX}plugins` VALUES ('1', 'wmzz_debug', '1', '');
 
 DROP TABLE IF EXISTS `{VAR-PREFIX}tieba`;
 CREATE TABLE `{VAR-PREFIX}tieba` (
-`id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `uid` int(30) NOT NULL,
   `pid` int(30) NOT NULL DEFAULT '0',
   `fid` int(30) NOT NULL DEFAULT '0',
@@ -44,109 +105,29 @@ CREATE TABLE `{VAR-PREFIX}tieba` (
   `no` int(10) NOT NULL DEFAULT '0',
   `status` int(10) NOT NULL DEFAULT '0',
   `lastdo` varchar(200) DEFAULT '0',
-  `last_error` text
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `{VAR-PREFIX}users`;
+  `last_error` text,
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;DROP TABLE IF EXISTS `{VAR-PREFIX}users`;
 CREATE TABLE `{VAR-PREFIX}users` (
-`id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `pw` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `role` enum('banned','vip','user','admin') NOT NULL DEFAULT 'user',
-  `t` varchar(20) NOT NULL DEFAULT 'tieba'
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+  `t` varchar(20) NOT NULL DEFAULT 'tieba',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `{VAR-PREFIX}users` VALUES ('1', 'admin', '66fa03f6ba652e2850d5e19d3a3fa9fc', 'kenvix@vip.qq.com', 'admin', 'tieba');
 
 DROP TABLE IF EXISTS `{VAR-PREFIX}users_options`;
 CREATE TABLE `{VAR-PREFIX}users_options` (
-`id` int(30) NOT NULL,
+  `id` int(30) NOT NULL AUTO_INCREMENT,
   `uid` int(30) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `value` text
+  `value` text,
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`) USING BTREE
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-ALTER TABLE `{VAR-PREFIX}baiduid`
- ADD PRIMARY KEY (`id`), ADD INDEX `uid` (`uid`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}cron`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}options`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}plugins`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}tieba`
- ADD PRIMARY KEY (`id`), ADD INDEX `uid` (`uid`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}users_options`
- ADD PRIMARY KEY (`id`), ADD INDEX `uid` (`uid`) USING BTREE;
-
-ALTER TABLE `{VAR-PREFIX}baiduid`
-MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `{VAR-PREFIX}cron`
-MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `{VAR-PREFIX}options`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `{VAR-PREFIX}plugins`
-MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `{VAR-PREFIX}tieba`
-MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `{VAR-PREFIX}users`
-MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `{VAR-PREFIX}users_options`
-MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-
-INSERT INTO `{VAR-PREFIX}cron` (`name`, `orde`, `file`, `no`, `desc`, `freq`, `lastdo`, `log`) VALUES('system_sign', 0, 'lib/cron_system_sign.php', 0, '每天对所有贴吧进行签到\r\n忽略或卸载此任务会导致停止签到', 0, '0', '');
-INSERT INTO `{VAR-PREFIX}cron` (`name`, `orde`, `file`, `no`, `desc`, `freq`, `lastdo`, `log`) VALUES('system_sign_retry', 1, 'lib/cron_system_sign_retry.php', 0, '对所有签到失败的贴吧进行复签\r\n忽略或卸载此任务会导致停止复签', 0, '0', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'system_url', '{VAR-SYSTEM-URL}');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'system_name', '贴吧云签到');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'protector', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_limit', '15');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'tb_max', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'sign_mode', 'a:2:{i:0;s:1:"1";i:1;s:1:"3";}');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'footer', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'enable_reg', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'protect_reg', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'yr_reg', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'icp', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'trigger', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_mode', 'MAIL');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_name', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_yourname', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_host', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_port', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_auth', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_smtpname', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_smtppw', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'fb', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cloud', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'enable_addtieba', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'pwdmode', 'md5(md5(md5($pwd)))');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'retry_max', '10');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_order', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'fb_tables', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'dev', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_last_do_time', '2015-02-13');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_last_do', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_isdoing', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_pw', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'sign_sleep', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cktime', '999999');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'bduss_num', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'sign_multith', '1');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'sign_asyn', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_asyn', '');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'cron_sign_again', 'a:2:{s:3:"num";i:0;s:6:"lastdo";s:10:"2015-02-13";}');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'sign_hour', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'mail_ssl', '0');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'freetable', 'tieba');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'core_version', '3.9');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'isapp', '{VAR-ISAPP}');
-INSERT INTO `{VAR-PREFIX}options` ( `name`, `value`) VALUES( 'baidu_name', '1');
-INSERT INTO `{VAR-PREFIX}plugins` ( `name`, `status`, `options`) VALUES( 'wmzz_debug', 1, '');
