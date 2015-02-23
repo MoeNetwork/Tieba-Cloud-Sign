3b6a265d26382a34ca68a9c5b7b189a333e13d37
无名智者 <kenvix@vip.qq.com>
2015-02-23 09:50:37 +0800

    修复bug

--- setting.php
+++ setting.php
@@ -64,7 +64,7 @@ switch (SYSTEM_PAGE) {
 		}
 		elseif (isset($_GET['install'])) {
 			if(!empty($_REQUEST['ver'])){
-				msg ('该插件仅适用于 V'.$_REQUEST['ver'].' 及以上的版本，您的云签到版本低于插件所需最低版本，是否强制安装（强制安装可能造成云签到损坏）<br/><br/><a href="setting.php?mod=admin:plugins&install='.$plugin.'">强制安装</a>　　<a href="setting.php?mod=admin:plugins">取消安装</a><br/>',false,true);
+				msg ('该插件仅适用于 V'.$_REQUEST['ver'].' 及以上的版本，您的云签到版本低于插件所需最低版本，是否强制安装（强制安装可能造成云签到损坏）<br/><br/><a href="setting.php?mod=admin:plugins&install='.$_GET['install'].'">强制安装</a>　　<a href="setting.php?mod=admin:plugins">取消安装</a><br/>',false,true);
 			}
 			installPlugin($_GET['install']);
 		}
