ecb00e66b4a04b5120974e44997e1dca6cf5320a
无名智者 <kenvix@vip.qq.com>
2015-03-16 22:20:41 +0800

    由于兼容性问题，取消在msg丢弃先前的缓冲区

--- lib/msg.php
+++ lib/msg.php
@@ -11,7 +11,6 @@ function msg($msg = '未知的异常',$url = true,$die = true) {
     } else {
         $sysname = SYSTEM_FN;
     }
-    ob_clean();
     ?>  
     <!DOCTYPE html>
     <html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">
