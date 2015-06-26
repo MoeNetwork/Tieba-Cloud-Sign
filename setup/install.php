3c74ab09b6aebed214435e721e744d87de24fe48
superxzr <a457418121@gmail.com>
2015-06-26 14:36:25 +0800

    修改了一处的描述，并提升版权保留的要求

--- setup/install.php
+++ setup/install.php
@@ -13,7 +13,7 @@ if (file_exists(SYSTEM_ROOT2.'/install.lock')) {
 }
 $csrf = !empty($_SERVER['HTTP_REFERER']) ? parse_url($_SERVER['HTTP_REFERER']) : '';
 if ( isset($_GET['step']) && ( empty($csrf['host']) || $csrf['host'] != $_SERVER['SERVER_NAME'] ) ) {
-	msg('安装程序检测到潜在的CSRF攻击，已被拦截，请点击返回重新操作','index.php');
+	msg('安装程序检测到来源异常，已被拦截，请按步骤进行安装！点击返回重新操作','index.php');
 }
 
 	echo '<!DOCTYPE html><html><head>';
@@ -47,7 +47,7 @@ if ( isset($_GET['step']) && ( empty($csrf['host']) || $csrf['host'] != $_SERVER
 	if (!isset($_GET['step']) || $_GET['step'] == 0) {
 		echo '<h2>阅读许可协议</h2><br/>';
 		echo '<iframe src="../license.html" style="width:100%;height:465px;"></iframe>';
-		echo '<br/><br/><input type="button" onclick="if(confirm(\'“我尊重原作者为云签事业付出的心血，在使用该永久免费的云签系统的同时将保护原作者的版权。\r\n保证原作者的名称、链接等版权信息不被删改或遮挡。如需二次开发，可添加相应的二次开发者版权信息，但不会删改或遮挡原作者版权信息。”\r\n\r\n同意请确定，不同意请取消\')){location = \'install.php?step=1\';} else {alert(\'请立即删除所有与本程序相关的文件及其延伸产品\');location = \'index.html\';}" class="btn btn-success" value="我接受">&nbsp;&nbsp;&nbsp;';
+		echo '<br/><br/><input type="button" onclick="if(confirm(\'“我尊重原作者为云签事业付出的心血，在使用该永久免费的云签系统的同时将保护原作者的版权。\r\n保证原作者的名称、链接等版权信息不被删改、淡化或遮挡。如需二次开发，可添加相应的二次开发者版权信息，但不会删改或遮挡原作者版权信息。”\r\n\r\n同意请确定，不同意请取消\')){location = \'install.php?step=1\';} else {alert(\'请立即删除所有与本程序相关的文件及其延伸产品\');location = \'index.html\';}" class="btn btn-success" value="我接受">&nbsp;&nbsp;&nbsp;';
 		echo '<input type="button" onclick=";alert(\'请立即删除所有与本程序相关的文件及其延伸产品\');location = \'index.html\';" class="btn btn-danger" value="我拒绝">';
 	} else {
 		switch (strip_tags($_GET['step'])) {
