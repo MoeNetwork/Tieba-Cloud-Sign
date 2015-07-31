5b526d249327fc3035b105790ad76d09b8f847b9
96dl <76729233@qq.com>
2015-07-31 23:27:43 +0800

    修复升级白屏问题（实际升级成功）

--- setup/update3.9to4.0.php
+++ setup/update3.9to4.0.php
@@ -33,6 +33,7 @@ MODIFY COLUMN `status`  tinyint(2) UNSIGNED NOT NULL DEFAULT 0 AFTER `no`;
 ',true);
         $m->query('ALTER TABLE `'.DB_PREFIX.$value.'`
 ADD INDEX `latest` (`latest`) USING BTREE ;',true);
+
     }
 
     $m->query('
@@ -41,7 +42,7 @@ MODIFY COLUMN `id`  int(30) UNSIGNED NOT NULL AUTO_INCREMENT FIRST ,
 MODIFY COLUMN `uid`  int(30) UNSIGNED NOT NULL AFTER `id`,
 ADD INDEX (`name`);
 ',true);
-    $m->query('
+   $m->query('
 ALTER TABLE `'.DB_PREFIX.'cron`
 ADD INDEX `name` (`name`) USING BTREE;
 ',true);
@@ -53,10 +54,10 @@ ADD INDEX `name` (`name`) USING BTREE ;
 ',true);
     $m->query('ALTER TABLE `'.DB_PREFIX.'plugins`
 ADD `ver` varchar(15) DEFAULT NULL,
-ADD `order` int(10) unsigned NOT NULL DEFAULT '0';
+ADD `order` int(10) unsigned NOT NULL DEFAULT 0;
 ',true);
 
     //------------------------------------------------//
-    unlink(__FILE__);
+    //unlink(__FILE__);
     option::set('core_version' , '4.0');
     msg('您的云签到已成功升级到 V4.0 版本，请立即删除 /setup/update3.9to4.0.php，谢谢<br/><br/>若要获取 V4.0 版本新特性，请前往 <a href="http://www.stus8.com/forum.php?mod=viewthread&tid=6411">StusGame GROUP</a> ', SYSTEM_URL);
