28c56bc0692a1a61182a26e24c7a6828db64abed
Kenvix <kenvix@vip.qq.com>
2016-05-21 14:27:13 +0800

    取消自动放置/setup/install.lock

--- init.php
+++ init.php
@@ -19,7 +19,7 @@ define('SUPPORT_URL', 'http://s.stus8.com/tcs/');
 require SYSTEM_ROOT.'/lib/msg.php';
 //如需停止站点运行，请解除注释，即删除开头的 //
 //msg('站点已关闭！请稍后再试，如有疑问请联系站长解决！');
-if (!file_exists(SYSTEM_ROOT.'/setup/install.lock') && file_exists(SYSTEM_ROOT.'/setup/install.php') && !file_put_contents(SYSTEM_ROOT.'/setup/install.lock', '1')) {
+if (!file_exists(SYSTEM_ROOT.'/setup/install.lock') && file_exists(SYSTEM_ROOT.'/setup/install.php')) {
 	msg('<h2>检测到无 install.lock 文件</h2><ul><li><font size="4">如果您尚未安装本程序，请<a href="./setup/install.php">前往安装</a></font></li><li><font size="4">如果您已经安装本程序，请手动放置一个空的 install.lock 文件到 /setup 文件夹下，<b>为了您站点安全，在您完成它之前我们不会工作。</b></font></li></ul><br/><h4>为什么必须建立 install.lock 文件？</h4>它是云签到的保护文件，如果云签到检测不到它，就会认为站点还没安装，此时任何人都可以安装/重装云签到。<br/><br/>',false,true,false);	
 }
 header("content-type:text/html; charset=utf-8");
