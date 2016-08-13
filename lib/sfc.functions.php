9f58299303534ee0f72dbf0437c5ffa7f9f9d621
Ver4 <76729233@qq.com>
2016-08-13 19:21:22 +0800

    修复因度娘作死无法绑定，签到问题

--- lib/sfc.functions.php
+++ lib/sfc.functions.php
@@ -81,10 +81,10 @@ function textMiddle($text, $left, $right) {
  * @return string|bool 百度用户名，失败返回FALSE
  */
 function getBaiduId($bduss){
-	global $m;
-	$header[] = 'Content-Type:application/x-www-form-urlencoded; charset=UTF-8';
-	$header[] = 'Cookie: BDUSS='.$bduss;
+	$header[] = 'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36';
+    $header[] = 'Content-Type: application/xhtml+xml; charset=utf-8';
 	$c = new wcurl('http://wapp.baidu.com/',$header);
+    $c->addCookie(array('BDUSS' => $bduss,'BAIDUID' => strtoupper(md5(time()))));
 	$data = $c->get();
 	$c->close();
 	return urldecode(textMiddle($data,'i?un=','">'));
