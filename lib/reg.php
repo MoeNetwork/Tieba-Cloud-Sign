2e93b3ce36c5f3765f242e2da5e75175f51fa4ca
cpuer <lastczj@gmail.com>
2021-12-23 17:18:14 +0800

    修复在PHP81环境中的warning
    修复在PHP81中warning：Deprecated: Automatic conversion of false to array is deprecated in /lib/reg.php on line 34

--- lib/reg.php
+++ lib/reg.php
@@ -31,7 +31,7 @@ while ($rsr = $m->fetch_array($rs)) {
 
 //贴吧分表列表
 $i['tabpart'] = $i['table'] = unserialize($i['opt']['fb_tables']);
-$i['table'][] = 'tieba'; //贴吧表列表
+$i['table'] = ['tieba']; //贴吧表列表
 
 //当前页面/模式, $i['mode'][0] 一般表示页面
 if (!empty($_REQUEST['mod'])) {
