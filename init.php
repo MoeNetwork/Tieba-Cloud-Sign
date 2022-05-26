95a00d3ddb55183bb4ac8d2d5dd54686c4f7daaa
BANKA2017 <kdwnil@hotmail.com>
2022-05-26 16:30:08 +0800

    update: version code

--- init.php
+++ init.php
@@ -5,7 +5,7 @@
  */
 
 define('SYSTEM_FN','百度贴吧云签到');
-define('SYSTEM_VER','4.97');
+define('SYSTEM_VER','4.98');
 define('SYSTEM_VER_NOTE','');
 define('SYSTEM_ROOT',dirname(__FILE__));
 define('PLUGIN_ROOT',dirname(__FILE__) . '/plugins/');
